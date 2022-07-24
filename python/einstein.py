9d5098984e90bd0635620d1157c9ce451af4a408
henok <ik@henok.nl>
2022-07-24 15:07:13 +0200

    Bugfixes plus performance improvement
    - Cache rotations
    - Fix coordinates of the 'b' block
    - Add debug information
    - Fix blocked positions for challenge 6

--- python/einstein.py
+++ python/einstein.py
@@ -1,6 +1,7 @@
 import numpy as np
 from collections import deque
-from time import time
+from copy import deepcopy
+from time import time, sleep
 import os
 
 EMPTY, FILLED, DISALLOWED = 0, 1, 2
@@ -11,13 +12,15 @@ def rot2d(degrees):
     c, s = np.cos(angle), np.sin(angle)
     return np.array([[c, -s], [s, c]])
 
+ROTATIONS = {x: rot2d(x) for x in [0, 90, 180, 270]}
+
 
 """Functions to return coordinates for the different blocks"""
 def get_a(x, y, rotation=0, mirror=False):
     A = np.array([[0, 0, 0, 0, 2, 2, 2, 2, 1, 1], 
                   [0, 1, 2, 3, 0, 1, 2, 3, 1, 3]])
-    r = rot2d(rotation)
-    A = (r @ A).astype('int')
+    r = ROTATIONS[rotation]
+    A = np.rint(r @ A).astype('int')
     A[0] += x
     A[1] += y
     
@@ -26,11 +29,11 @@ def get_a(x, y, rotation=0, mirror=False):
 def get_t(x, y, rotation=0, mirror=False):
     A = np.array([[0, 1, 1, 1, 1, 2, 2],
                   [2, 0, 1, 2, 3, 0, 2]])
-    r = rot2d(rotation)
+    r = ROTATIONS[rotation]
     if mirror:
         A[0] = -A[0]
         A[0] += 3
-    A = (r @ A).astype('int')
+    A = np.rint(r @ A).astype('int')
     A[0] += x
     A[1] += y
     
@@ -39,11 +42,11 @@ def get_t(x, y, rotation=0, mirror=False):
 def get_l(x, y, rotation=0, mirror=False):
     A = np.array([[0, 0, 0, 0, 1],
                   [0, 1, 2, 3, 0]])
-    r = rot2d(rotation)
+    r = ROTATIONS[rotation]
     if mirror:
         A[0] = -A[0]
         A[0] += 2
-    A = (r @ A).astype('int')
+    A = np.rint(r @ A).astype('int')
     A[0] += x
     A[1] += y
     
@@ -52,11 +55,11 @@ def get_l(x, y, rotation=0, mirror=False):
 def get_big_l(x, y, rotation=0, mirror=False):
     A = np.array([[0, 0, 0, 0, 1, 2],
                   [0, 1, 2, 3, 0, 0]])
-    r = rot2d(rotation)
+    r = ROTATIONS[rotation]
     if mirror:
         A[0] = -A[0]
         A[0] += 3
-    A = (r @ A).astype('int')
+    A = np.rint(r @ A).astype('int')
     A[0] += x
     A[1] += y
     
@@ -65,8 +68,8 @@ def get_big_l(x, y, rotation=0, mirror=False):
 def get_u(x, y, rotation=0, mirror=False):
     A = np.array([[0, 0, 0, 1, 2, 2, 2],
                   [0, 1, 2, 0, 0, 1, 2]])
-    r = rot2d(rotation)
-    A = (r @ A).astype('int')
+    r = ROTATIONS[rotation]
+    A = np.rint(r @ A).astype('int')
     A[0] += x
     A[1] += y
     
@@ -75,8 +78,8 @@ def get_u(x, y, rotation=0, mirror=False):
 def get_i(x, y, rotation=0, mirror=False):
     A = np.array([[0, 0, 0],
                   [0, 1, 2]])
-    r = rot2d(rotation)
-    A = (r @ A).astype('int')
+    r = ROTATIONS[rotation]
+    A = np.rint(r @ A).astype('int')
     A[0] += x
     A[1] += y
     
@@ -85,11 +88,11 @@ def get_i(x, y, rotation=0, mirror=False):
 def get_s(x, y, rotation=0, mirror=False):
     A = np.array([[0, 0, 1, 1],
                   [0, 1, 1, 2]])
-    r = rot2d(rotation)
+    r = ROTATIONS[rotation]
     if mirror:
         A[0] = -A[0]
         A[0] += 2
-    A = (r @ A).astype('int')
+    A = np.rint(r @ A).astype('int')
     A[0] += x
     A[1] += y
     
@@ -98,11 +101,11 @@ def get_s(x, y, rotation=0, mirror=False):
 def get_e(x, y, rotation=0, mirror=False):
     A = np.array([[0, 0, 1, 1, 2],
                   [0, 1, 1, 2, 2]])
-    r = rot2d(rotation)
+    r = ROTATIONS[rotation]
     if mirror:
         A[0] = -A[0]
         A[0] += 3
-    A = (r @ A).astype('int')
+    A = np.rint(r @ A).astype('int')
     A[0] += x
     A[1] += y
     
@@ -110,12 +113,12 @@ def get_e(x, y, rotation=0, mirror=False):
 
 def get_b(x, y, rotation=0, mirror=False):
     A = np.array([[0, 0, 0, 0, 1, 1],
-                  [0, 0, 0, 0, 2, 3]])
-    r = rot2d(rotation)
+                  [0, 1, 2, 3, 2, 3]])
+    r = ROTATIONS[rotation]
     if mirror:
         A[0] = -A[0]
         A[0] += 2
-    A = (r @ A).astype('int')
+    A = np.rint(r @ A).astype('int')
     A[0] += x
     A[1] += y
     
@@ -169,7 +172,7 @@ def solve(board):
 
     previous_changes = deque()
     blocks, block_index = get_blocks(), 0
-    now, counter = time(), 0
+    now, total_time, counter = time(), 0, 0
     N = 1000
 
     while block_index < 14:
@@ -181,13 +184,15 @@ def solve(board):
             print()
             print(f"Round {counter}")
             print(f"\t Placed blocks\t\t: {block_index}") 
-            print(f"\t Invalid placements\t: {round(invalid/counter*100)}%")
-            print(f"\t Occupied placements\t: {round(not_empty/counter*100, 3)}%")
+            print(f"\t Invalid placements\t: {round(invalid/counter*100,1)}%")
+            print(f"\t Occupied placements\t: {round(not_empty/counter*100,1)}%")
             
             t, now = time() - now, time()
+            total_time += t
             print(f"\t Time per {N} rounds\t: {round((t), 3)}s")
+            print(f"\t Avg time per n rounds\t: {round(total_time/(counter/N), 3)}s")
             print(f"\t Iterations per second\t: {round(1 / t * N)}")
-
+            print(f"\t Free squares\t\t: {sum([sum([1 if x == 0 else 0 for x in y]) for y in board])}")
             print()
             print(f"Board state ({EMPTY} = EMPTY, {FILLED} = FILLED, {DISALLOWED} = DISALLOWED)\n")
             print_board(board)
@@ -203,6 +208,7 @@ def solve(board):
 
             # Restore previous board state
             changes = previous_changes.pop()
+            # print("Backtracking", len(changes))
             for x, y in changes:
                 board[y][x] = EMPTY
             # print_board(board)
@@ -216,7 +222,7 @@ def solve(board):
         coords = blocks[block_index](*params)
         
         # Check if block is entirely on the board
-        if np.any(coords[coords < 0]) or np.any(coords[coords >= 10]):
+        if np.min(coords) < 0 or np.max(coords) >= 10:
             invalid += 1
             continue
         
@@ -226,20 +232,18 @@ def solve(board):
         for x, y in coords.T:
             if board[y][x] != EMPTY:
                 not_empty += 1
-
-                # Restore board state if blocks overlap
-                for x, y in changes:
-                    board[y][x] = EMPTY
                 break
-    
-            board[y][x] = FILLED
             changes.append((x, y))
 
         # Move to next block if placement is succesful
         else:
 
+            # Apply changes to board state if blocks overlap
+            for x, y in changes:
+                board[y][x] = FILLED
+
             # Store changes so board state can be restored when backtracking
-            previous_changes.append(changes)
+            previous_changes.append(changes.copy())
 
             # Check if solution is found
             # NOTE: not necessary for challenge 6, but in general not all
@@ -254,16 +258,23 @@ def solve(board):
             current_configuration[block_index] = params
             
             block_index += 1
+            # print(f"\t Free squares\t\t: {sum([sum([1 if x == 0 else 0 for x in y]) for y in board])}")
+            # sleep(2)
             if done:
                 print(f"{counter}: DONE")
                 print_result(current_configuration)
                 break
+    print("DONE")
+    print_board(board)
+    print_result(current_configuration)
 
 def challenge6():
+    # print(get_t(3, 2, 90, True))
+    # exit()
     """Solve challenge 6"""
     disallowed = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0),
                   (0, 1), (1, 1), (2, 1), (3, 1),
-                  (0, 2), (1, 2), (1, 2),
+                  (0, 2), (1, 2), (2, 2),
                   (0, 3), (1, 3),
                   (0, 4),
                   (4, 4),
@@ -271,6 +282,8 @@ def challenge6():
     board = [[EMPTY for _ in range(10)] for _ in range(10)]
     for x, y in disallowed:
         board[y][x] = DISALLOWED
+    print(f">\t Free squares\t\t: {sum([sum([1 if x == EMPTY else 0 for x in y]) for y in board])}")
+    
     solve(board)
 
 if __name__ == '__main__':
